bf4f4deba3cabaca0af1fa8b227812ad279b22e5
skechung <47089981+skechung@users.noreply.github.com>
2020-08-09 11:22:40 -0700

    Add files via upload
    To get this to work, you must have cv2 and numpy installed, also an image called test.png

---
+++ HexToWeights.py
@@ -0,0 +1,110 @@
+
+import numpy as py
+import cv2
+
+#testing image path
+path = './test.png'
+image = cv2.imread(path)
+
+#class of pixel
+class Pixel():
+
+    #initialize itself
+    def __init__(self):
+        #RGB
+        self.R = 0
+        self.G = 0
+        self.B = 0
+        #HSV
+        self.H = 0 #Hue
+        self.S = 0 #Saturation
+        self.V = 0 #Value
+
+    def getR(self, R):
+        self.R = R
+    def getG(self, G):
+        self.G = G
+    def getB(self, B):
+        self.B = B
+
+    def R(self):
+        return self.R
+    def G(self):
+        return self.G
+    def B(self):
+        return self.B
+
+
+    def getH(self, H):
+        self.H = H
+    def getS(self, S):
+        self.S = S
+    def getV(self, V):
+        self.V = V
+
+    def H(self):
+        return self.H
+    def S(self):
+        return self.S
+    def V(self):
+        return self.V
+
+#takes "in" an image and iterates over every pixel
+#and returns an array of pixel objects
+def getRGB(image):
+    pixelArr = []
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            #new pixel object
+            newPixel = Pixel()
+            #image[i,j] returns the BRG value of that pixel
+            newPixel.getR(image[i,j][2])
+            newPixel.getG(image[i,j][1])
+            newPixel.getB(image[i,j][0])
+            #append new pixel object to pixelArr
+            pixelArr.append(newPixel)
+    return pixelArr
+
+#takes in an array of pixel objects
+#and defines its HSV values
+def getHSV(pixelArr):
+    for i in pixelArr:
+        rPrime = i.R/255
+        gPrime = i.G/255
+        bPrime = i.B/255
+
+        cMax = max(rPrime, gPrime, bPrime)
+        cMin = min(rPrime, gPrime, bPrime)
+        delta = cMax - cMin
+
+        H = 0
+        S = 0
+        V = 0
+        
+        #hue calculation
+        if delta == 0:
+            H = 0
+        elif cMax == rPrime:
+            H = (60)*(((gPrime-bPrime)/delta)%6)
+        elif cMax == gPrime:
+            H = (60)*(((bPrime-rPrime)/delta)+2)
+        elif cMax == bPrime:
+            H = (60)*(((rPrime-gPrime)/delta)+4)
+
+        #saturation calculation:
+        if cMax == 0:
+            S = 0
+        else:
+            S = delta/cMax
+
+        #value calculation:
+        V = cMax
+
+        #set the HSV values into the pixelArr
+        i.getH(H)
+        i.getS(S)
+        i.getV(V)
+
+           
+pixelArr = getRGB(image)
+getHSV(pixelArr) 
